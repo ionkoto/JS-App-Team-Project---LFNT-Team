3837d0558fd8235aba185964fbb5cda4a6ab418b
Yoan Petrushinov <i_onk_o@abv.bg>
2016-12-04 23:41:20 +0200

    movie fix
    movie.js fix

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -5,7 +5,6 @@ import './Movie.css';
 export default class Movie extends Component {
 
     render() {
-        let linkBtn = <Link to={"/movies/" + this.props.id} className="btn btn-group-sm">Details</Link>;
         return(
             <tr>
                 <td>{this.props.title}</td>
@@ -14,8 +13,7 @@ export default class Movie extends Component {
                 <td>{this.props.genre || 'No genre'}</td>
                 <td>{this.props.rating || 'No rating'}</td>
                 <td>{this.props.date || 'No date'}</td>
-                <td>{linkBtn}</td>
-                {/*<Link to={"/movies/" + this.props.id} className="btn btn-group-sm">Details</Link>*/}
+                <Link to={"/movies/" + this.props.id} className="btn btn-group-sm">Details</Link>
             </tr>
         )
     }
