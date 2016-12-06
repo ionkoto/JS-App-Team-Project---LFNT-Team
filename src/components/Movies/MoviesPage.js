a8c44b767cc32aadca2e3a6f0655a4febb885936
Yoan Petrushinov <i_onk_o@abv.bg>
2016-12-06 16:22:05 +0200

    Movies Table Logic change
    Movies Table Logic change + Show 6 insted of 5 movies on home page +
    Delete Table Component

--- src/components/Movies/MoviesPage.js
+++ src/components/Movies/MoviesPage.js
@@ -5,11 +5,12 @@ import {Link} from 'react-router';
 //import observer from '../../models/observer';
 //import Reactable from 'reactable';
 import Reactable from '../common/reactable';
-import TableTemplate from '../common/Table';
 
 let Table = Reactable.Table;
 let Thead = Reactable.Thead;
 let Th = Reactable.Th;
+let Tr = Reactable.Tr;
+let Td = Reactable.Td;
 
 export default class MoviesPage extends Component {
     constructor(props) {
@@ -53,48 +54,42 @@ export default class MoviesPage extends Component {
 
     render() {
         let data = this.addActions();
-
         return (
 
             <div>
                 <h1>Movie Page</h1>
                 <Link to="/create" className="btn btn-info" >Create movie</Link>
-                {/*<div>*/}
-                    {/*<table className="table-striped">*/}
-                        {/*<thead>*/}
-                            {/*<tr>*/}
-                                {/*<td>Title</td>*/}
-                                {/*<td>Summary</td>*/}
-                                {/*<td>Director</td>*/}
-                                {/*<td>Genre</td>*/}
-                                {/*<td>Rating (1/10)</td>*/}
-                                {/*<td>Release Date</td>*/}
-                                {/*<td>Actions</td>*/}
-                            {/*</tr>*/}
-                        {/*</thead>*/}
-                        {/*<tbody>*/}
-                        {/*{this.state.movies.map((e, i) => {*/}
-                            {/*return <Movie key={i}*/}
-                                          {/*id={e._id}*/}
-                                          {/*title={e.title}*/}
-                                          {/*summary={e.summary}*/}
-                                          {/*director={e.director}*/}
-                                          {/*genre={e.genre}*/}
-                                          {/*rating={e.rating}*/}
-                                          {/*date={e.date}/>*/}
-                        {/*})}*/}
-                        {/*</tbody>*/}
-                    {/*</table>*/}
-                {/*</div>*/}
-                <TableTemplate
-                    className="table"
-                    filterable={['title', 'summary', 'director', 'genre', 'rating','date']}
-                    noDataText="No matching records found"
-                    itemsPerPage={5}
-                    currentPage={0}
-                    sortable={true}
-                    data={data}
-                   />
+                <Table className="table"
+                       filterable={['title', 'summary', 'director', 'genre', 'rating','date']}
+                       noDataText="No matching records found"
+                       itemsPerPage={5}
+                       currentPage={0}
+                       sortable={true}
+                       data={data}>
+                    <Thead>
+                    <Th column="title">
+                        <em className="title-header">Title</em>
+                    </Th>
+                    <Th column="summary">
+                        <em className="summary-header">Summary</em>
+                    </Th>
+                    <Th column="director">
+                        <em className="director-header">Director</em>
+                    </Th>
+                    <Th column="genre">
+                        <em className="genre-header">Genre</em>
+                    </Th>
+                    <Th column="rating">
+                        <em className="rating-header">Rating</em>
+                    </Th>
+                    <Th column="date">
+                        <em className="date-header">Release date</em>
+                    </Th>
+                    <Th column="actions">
+                        <em className="actions-header">Details</em>
+                    </Th>
+                    </Thead>
+                </Table>
             </div>
         );
     }
--- src/components/Movies/MyMoviesPage.js
+++ src/components/Movies/MyMoviesPage.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-import Movie from './Movie';
 import {loadMovies} from '../../models/movie';
 import {Link} from 'react-router';
 //import observer from '../../models/observer';
 //import Reactable from 'reactable';
 import Reactable from '../common/reactable';
-import TableTemplate from '../common/Table';
+
 
 let Table = Reactable.Table;
 let Thead = Reactable.Thead;
@@ -65,15 +64,37 @@ export default class MyMoviesPage extends Component {
                 <div>
                     <h1>My Movies Page</h1>
                     <Link to="/create" className="btn btn-info">Create movie</Link>
-                    <TableTemplate
-                        className="table"
-                        filterable={['title', 'summary', 'director', 'genre', 'rating','date']}
-                        noDataText="No matching records found"
-                        itemsPerPage={5}
-                        currentPage={0}
-                        sortable={true}
-                        data={data}
-                    />
+                    <Table className="table"
+                           filterable={['title', 'summary', 'director', 'genre', 'rating','date']}
+                           noDataText="No matching records found"
+                           itemsPerPage={5}
+                           currentPage={0}
+                           sortable={true}
+                           data={data}>
+                        <Thead>
+                        <Th column="title">
+                            <em className="title-header">Title</em>
+                        </Th>
+                        <Th column="summary">
+                            <em className="summary-header">Summary</em>
+                        </Th>
+                        <Th column="director">
+                            <em className="director-header">Director</em>
+                        </Th>
+                        <Th column="genre">
+                            <em className="genre-header">Genre</em>
+                        </Th>
+                        <Th column="rating">
+                            <em className="rating-header">Rating</em>
+                        </Th>
+                        <Th column="date">
+                            <em className="date-header">Release date</em>
+                        </Th>
+                        <Th column="actions">
+                            <em className="actions-header">Details</em>
+                        </Th>
+                        </Thead>
+                    </Table>
                 </div>
             );
         } else
