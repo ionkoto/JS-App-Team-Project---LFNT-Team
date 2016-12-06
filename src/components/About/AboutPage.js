4ba7b873c9890e9266c3ec34d56aee6d1d2c3e11
Yoan Petrushinov <i_onk_o@abv.bg>
2016-12-06 22:49:13 +0200

    Revert "Home and About page final fixes"
    This reverts commit 83dbc39547920711d6bdf09cdfe629bdf72ed091.

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -4,12 +4,12 @@ import React, {Component} from 'react';
 export default class AboutPage extends Component {
     render() {
         return (
-            <div className="jumbotron">
-                <h2>About LFNT Movie Database</h2>
-                <p>The LFNT Movie Database is an online database of information related to films, television programs including production crew, fictional characters, biographies, plot summaries, reviews etc. operated by LFNT Team - Software University</p>
+            <div class="jumbotron">
+                <h1>About LFNT Movie Database</h1>
+                <p>The Internet Movie Database (abbreviated IMDb) is an online database of information related to films, television programs and video games, including cast, production crew, fictional characters, biographies, plot summaries, trivia and reviews, operated by IMDb.com, Inc., a subsidiary of Amazon.com.</p>
                     <p> Actors and crew can post their own résumé and upload photos of themselves for a yearly fee. U.S. users can view over 6,000 movies and television shows from CBS, Sony, and various independent filmmakers.</p>
-                        <p> Launched in 2015 by computer programmer Pesho Peshev, the company was incorporated in Bulgaria as LFNT Movie Database Ltd with revenue generated through advertising, licensing and partnerships. In 2016 it became a subsidiary of Google.com, who were then able to use it as an advertising resource for selling DVDs and videotapes.</p>
-                            <p> As of December 2016, LFNT Movie Database has approximately 10 titles (including episodes) and 7.6 personalities in its database,[2] as well as 17 million registered users. It is ranked 58 in Alexa Top Global sites ranking.</p>
+                        <p> Launched in 1990 by computer programmer Col Needham, the company was incorporated in the UK as Internet Movie Database Ltd in 1996 with revenue generated through advertising, licensing and partnerships. In 1998 it became a subsidiary of Amazon.com, who were then able to use it as an advertising resource for selling DVDs and videotapes.</p>
+                            <p> As of December 2016, IMDb has approximately 4.0 million titles (including episodes) and 7.6 million personalities in its database,[2] as well as 70 million registered users. It is ranked 58 in Alexa Top Global sites ranking.</p>
                                 <p> The site enables registered users to submit new material and edits to existing entries. Although all data is claimed to be checked before going live, the system is in fact open to abuse and errors are rampant, especially in its catalog of older films. There is also much fancruft, including chat boards for every film and personality as well as general subject matter such as politics and religion. Users are also invited to rate any film on a scale of 1 to 10, and the totals are converted into a weighted mean-rating that is displayed beside each title, with online filters employed to deter ballot-stuffing.</p>
             </div>
         );
