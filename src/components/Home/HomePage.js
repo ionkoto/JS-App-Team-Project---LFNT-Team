83dbc39547920711d6bdf09cdfe629bdf72ed091
Yoan Petrushinov <i_onk_o@abv.bg>
2016-12-06 22:44:26 +0200

    Home and About page final fixes
    Home and About page final fixes

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -54,26 +54,28 @@ export default class HomePage extends Component {
         return (
             <div>
                 <div>{message}</div>
-                <h1>Last added: </h1>
+
+                    <h3 className="text-center">Latest: </h3>
+
                     {this.state.movies.map((e, i) => {
-                        return (
-                        <div key={i}>
-                                <article className="post col-md-6" key={i}>
-                                    <h3>{e.title}</h3>
-                                    <div className="about">
-                                        <b>Director: </b> <i>{e.director}</i><br/><b>Genre: </b><i>{e.genre}</i><br/><b>Rating (0/10): </b> <i>{e.rating}</i><br/><b>Release Date: </b><i>{e.date}</i>
-                                    </div>
-                                    <div className="body"><b>Summary: </b> <i>{e.summary}</i></div>
+                            return (
+                                <div key ={i} className="post col-md-6">
+                                    <article className="alert alert-dismissible alert-info">
+                                        <h3>{e.title}</h3>
+                                        <div className="about">
+                                            <b>Director: </b> <i>{e.director}</i><br/><b>Genre: </b><i>{e.genre}</i><br/><b>Rating (0/10): </b> <i>{e.rating}</i><br/><b>Release Date: </b><i>{e.date}</i>
+                                        </div>
+                                        <div className="body"><b>Summary: </b> <i>{e.summary}</i></div>
 
-                                    <div className="post">
-                                        <b>Poster</b><br/>
-                                        {(e.image !== "" )?<img src={e.image}/>:<div>Poster missing</div>}
-                                    </div>
+                                        <div className="post">
+                                            <b>Poster</b><br/>
+                                            {(e.image !== "" )?<img src={e.image}/>:<div>Poster missing</div>}
+                                        </div>
 
 
-                                </article>
-                        </div>
-                        );
+                                    </article>
+                                </div>
+                            );
                     })}
 
             </div>
